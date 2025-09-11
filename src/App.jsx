891d50f4f69d5e9355383785e449966b14fcfa73
Tarasdeveloper <tomahavki1@gmail.com>
2025-09-11 21:26:38 +0300

    started

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/Header';
+import HomePage from './components/HomePage';
+
+export default function App() {
+    return (
+        <div className="flex flex-col max-w-[1000px] mx-auto w-full">
+            <section className="min-h-screen flex flex-col">
+                <Header />
+                <HomePage />
+            </section>
+
+            <footer></footer>
+        </div>
+    );
+}
