e70e5afc242dde8285a57c41df8de92c2a3a3283
Tarasdeveloper <tomahavki1@gmail.com>
2025-09-26 13:43:29 +0300

    git added

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,4 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-    // optimizeDeps: {
-    //     include: ['@xenova/transformers'],
-    // },
-    // build: {
-    //     sourcemap: false, // Отключаем sourcemaps для избежания ошибок
-    // },
-    // server: {
-    //     fs: {
-    //         allow: ['..'],
-    //     },
-    // },
 });
